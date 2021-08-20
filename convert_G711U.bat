ce3d8dba4f9af425a4f54af3bdaae329f19c6baa
Elompenta <lutz@baebeca.de>
2021-08-20 15:48:17 +0200

    build 1001

---
+++ convert_G711U.bat
@@ -0,0 +1,8 @@
+rem You can then drag and drop a selection of G711u files onto the
+rem batch file (or onto a `short-cut' to it).
+rem The wav files end up in a folder called 'wav'.
+
+cd %~dp0
+mkdir wav
+FOR %%A IN (%*) DO sox\sox.exe --channels 1 --type raw --rate 8000 -U %%A wav\%%~nA.wav
+pause
---
+++ readme.txt
@@ -0,0 +1,5 @@
+You can then drag and drop a selection of G711a files onto the batch file (or onto a `short-cut' to it). The wav files end up in a folder called 'wav'.
+
+Also from command line the batch file can be used:
+
+C:temp> convert_G711A.bat input_file.g711a
